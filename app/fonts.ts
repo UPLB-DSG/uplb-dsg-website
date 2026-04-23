e40b160e4ce994e9866db53d66f3bf1c01a25d1c
smmariquit <smmariquit@up.edu.ph>
2026-04-23 18:36:17 +0800

    refactor: update branding, design tokens, and font configuration with Inter and Telegraf font families

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,16 +1,22 @@
 import localFont from 'next/font/local';
 
-export const telegrafRegular = localFont({
-    src: './fonts/Telegraf Regular.otf',
-    variable: '--font-telegraf-regular',
-})
-
-export const telegrafUltraBold = localFont({
-    src: './fonts/Telegraf UltraBold 800.otf',
-    variable: '--font-telegraf-ultra-bold',
-})
-
-export const telegrafUltraLight = localFont({
-    src: './fonts/Telegraf UltraLight 200.otf',
-    variable: '--font-telegraf-ultra-light',
-})
+export const telegraf = localFont({
+    src: [
+        {
+            path: './fonts/Telegraf Regular.otf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Telegraf UltraBold 800.otf',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Telegraf UltraLight 200.otf',
+            weight: '200',
+            style: 'normal',
+        },
+    ],
+    variable: '--font-telegraf',
+});
