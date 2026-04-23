5c01ee8dfea2d5a1cc28417c39fc8561cd878322
smmariquit <smmariquit@up.edu.ph>
2026-04-23 18:30:14 +0800

    feat: add Telegraf font definitions

---
+++ app/fonts.ts
@@ -0,0 +1,16 @@
+import localFont from 'next/font/local';
+
+export const telegrafRegular = localFont({
+    src: './fonts/Telegraf Regular.otf',
+    variable: '--font-telegraf-regular',
+})
+
+export const telegrafUltraBold = localFont({
+    src: './fonts/Telegraf UltraBold 800.otf',
+    variable: '--font-telegraf-ultra-bold',
+})
+
+export const telegrafUltraLight = localFont({
+    src: './fonts/Telegraf UltraLight 200.otf',
+    variable: '--font-telegraf-ultra-light',
+})
